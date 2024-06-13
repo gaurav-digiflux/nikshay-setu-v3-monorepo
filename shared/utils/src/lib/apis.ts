5fb79af8305b84ce3a2c4b7a71fef1f0a20e3c4a
GAURAV RATHVA <gauravr@digiflux.io>
2024-06-13 17:04:50 +0530

    lint test

--- shared/utils/src/lib/apis.ts
+++ shared/utils/src/lib/apis.ts
@@ -1,13 +1,12 @@
-import networkClient from "./networkClient";
-import { BASE_URL, urls } from "@nikshay-setu-v3-monorepo/constants";
+import networkClient from './networkClient'
+import { BASE_URL, urls } from '@nikshay-setu-v3-monorepo/constants'
 
 export async function getUserApi() {
-    return networkClient.get(
-        `${BASE_URL}${urls.GET_USER}`,
-        // 'https://dummyjson.com/users',
-        (status, response) => {
-            console.log('SIHI',response);
-            return response;
-        },
-    );
+  return networkClient.get(
+    `${BASE_URL}${urls.GET_USER}`,
+    (status, response) => {
+      console.log('SIHI', response)
+      return response
+    }
+  )
 }

665f3d687b7ea879032fc0da28316443fa260506
GAURAV RATHVA <gauravr@digiflux.io>
2024-06-13 16:07:34 +0530

    added husky test

--- shared/store/src/store/store.js
+++ shared/store/src/store/store.js
@@ -1,15 +1,15 @@
-import rootSaga from '../saga';
-import { rootReducer } from '../reducer';
-import { applyMiddleware, legacy_createStore } from 'redux';
-import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../saga'
+import { rootReducer } from '../reducer'
+import { applyMiddleware, legacy_createStore } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 export function initStore() {
-  const sagaMiddleware = createSagaMiddleware();
+  const sagaMiddleware = createSagaMiddleware()
   const store = legacy_createStore(
     rootReducer,
     {},
     applyMiddleware(sagaMiddleware)
-  );  
-  store.sagaTask = sagaMiddleware.run(rootSaga);
-  return store;
+  )
+  store.sagaTask = sagaMiddleware.run(rootSaga)
+  return store
 }
